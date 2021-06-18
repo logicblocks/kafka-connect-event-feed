d1914a83232faed3adfa37461ade8b1803bcf5c4
jonassvalin <jonassvalin@gmail.com>
2021-06-18 18:24:31 +0200

    Add test for multiple events

--- test/integration/kafka/connect/event_feed/happy_path_test.clj
+++ test/integration/kafka/connect/event_feed/happy_path_test.clj
@@ -115,6 +115,54 @@
           (is (= (haljson/resource->map event-resource)
                 message-payload)))))))
 
+(deftest fetches-multiple-events-from-event-feed
+  (let [kafka (ktc/kafka @kafka-atom)
+        kafka-connect (ktc/kafka-connect @kafka-atom)
+        wiremock-server @wiremock-atom
+
+        topic-name "events"
+
+        discovery-href (wms/url wiremock-server "/")
+        discovery-resource
+        (-> (hal/new-resource discovery-href)
+          (hal/add-link :events
+            {:href      (wms/url wiremock-server "/events{?since,pick}")
+             :templated true}))
+
+        event-id-1 (test-data/random-event-id)
+        event-href-1 (wms/url wiremock-server (str "/events/" event-id-1))
+        event-resource-1 (hal/new-resource event-href-1)
+
+        event-id-2 (test-data/random-event-id)
+        event-href-2 (wms/url wiremock-server (str "/events/" event-id-2))
+        event-resource-2 (hal/new-resource event-href-2)
+
+        events-resource
+        (-> (hal/new-resource (wms/url wiremock-server "/events"))
+          (hal/add-href :discovery discovery-href)
+          (hal/add-href :events event-href-1)
+          (hal/add-href :events event-href-2)
+          (hal/add-resource :events event-resource-1)
+          (hal/add-resource :events event-resource-2))]
+    (with-connector kafka-connect
+      {:name   "event-feed-source"
+       :config {"connector.class"         connector-class
+                "topic.name"              topic-name
+                "eventfeed.discovery.url" discovery-href}}
+      (wmc/with-stubs
+        [{:server wiremock-server
+          :req    [:GET "/"]
+          :res    [200 {:body (haljson/resource->json discovery-resource)}]}
+         {:server wiremock-server
+          :req    [:GET "/events"]
+          :res    [200 {:body (haljson/resource->json events-resource)}]}]
+        (let [messages (test-consumer/consume-n kafka topic-name 2)
+              message-payloads (map #(get-in % [:value :payload])
+                                 messages)]
+          (is (= [(haljson/resource->map event-resource-1)
+                  (haljson/resource->map event-resource-2)]
+                message-payloads)))))))
+
 ; full page
 ; many pages
 
